84c75fa2fb4493f547b4176ba28ca8d8d6fae630
mohamed <midoelharery@gmail.com>
2023-02-04 23:27:49 +0200

    New page

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// function Hello() { 
+//     alert("================\n== Hello Midooo! ==\n================")
+//     alert("5 + 5 = ?")
+//     let n = prompt("Enter Right Answer: ")
+//     if (n == 10) {
+//         alert("================\n==== Good Job! ====\n================")
+//     }
+//     else {
+//         alert("Noo It Is Wrong Answer :(")
+//     }
+//     while (n != 10) {
+//         let n = prompt("Enter Right Answer: ")
+//         if (n == 10) {
+//             alert("================\n==== Good Job! ====\n================")
+//             break
+//         }
+//         else {
+//             alert("Noo It Is Wrong Answer :(")
+
+//         }
+        
+//     }
+// }
+
+// let imgs = document.querySelectorAll(".activity div img");
+// for(let i = 0; i < imgs.length; i++){
+//     imgs[i].style = 'margin-right: 0px; width: 50px;'
+// }
+// window.alert('')
